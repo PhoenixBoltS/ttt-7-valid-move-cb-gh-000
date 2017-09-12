460ba8ef3e5571ee0adf062a9bf534693db7986f
Kartikey Soni <phoenixboltstudios@gmail.com>
2017-09-12 19:18:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,5 +11,6 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board, index)
+  index.to_i
 ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
 end
