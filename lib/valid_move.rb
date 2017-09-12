c6836d32b93c0a7f0f30efea12d30ac4d44501d8
Kartikey Soni <phoenixboltstudios@gmail.com>
2017-09-12 19:11:33 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,9 @@
 def valid_move(board, index)
       index = board[index - 1]
   if position_taken?
+    true
+  else
+    false
   end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
